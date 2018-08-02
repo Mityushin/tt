8b7ea43cedaa46714bc19871f58f084bb060a9a0
Дмитрий Митюшин <mityushin.dmitry@gmail.com>
2018-08-02 15:26:19 +0300

    done second part tasks

--- src/main/java/sp/SecondPartTasks.java
+++ src/main/java/sp/SecondPartTasks.java
@@ -1,20 +1,74 @@
 package sp;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
-    private SecondPartTasks() {}
+    private SecondPartTasks() {
+    }
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+
+        List<String> result = new ArrayList<>();
+
+        for (String strPath : paths) {
+
+            Path path = Paths.get(strPath);
+
+            try (Stream<String> lineStream = Files.lines(path)) {
+
+                result.addAll(
+                        lineStream
+                                .filter(x -> x.contains(sequence))
+                                .collect(Collectors.toList()));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        (Map.Entry<String, List<String>> x) -> x.getValue()
+                                .stream()
+                                .mapToInt((String y) -> {
+                                    Path path = Paths.get(y);
+                                    int result = 0;
+                                    try (Stream<String> lineStream = Files.lines(path)) {
+                                        result = lineStream
+                                                .mapToInt(String::length)
+                                                .sum();
+
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return result;
+                                })
+                                .sum()
+                ))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue(Integer::compare))
+                .map(Map.Entry::getKey)
+                .get();
     }
 }
